f7a90fbf316088a282d99e7a3f0263a99fea97e4
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-08 22:33:49 +0300

    ft:create a file forms.py

--- insta/forms.py
+++ insta/forms.py
@@ -1,9 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,NewPostForm
 from django.contrib.auth.models import User
-
-
+from django import forms
+from.models import *
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','password1','password2','email']
-    
+        
+        
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user', 'date_posted']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }        
